cebcffe4e6e2668e40ea700c81ed6f353c4f3b3a
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2025-01-31 11:21:37 +0700

    refactor(web): combine component

--- packages/web/src/views/HomeView.vue
+++ packages/web/src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 import { client } from '@/utils/api';
-import HelloWorld from '@/components/HelloWorld.vue';
 
 onMounted(async () => {
   response.value = await hello.index.get();
@@ -12,7 +11,30 @@ const response = ref<Awaited<ReturnType<typeof hello.index.get>> | null>(null);
 </script>
 
 <template>
-  <HelloWorld msg="You did it!" />
+  <div>
+    <h1 class="tw:text-center tw:block tw:text-emerald-600">You did it!</h1>
+    <div class="tw:grid tw:grid-cols-12 tw:gap-4">
+      <p class="tw:col-span-6 tw:col-start-4 tw:p-4 tw:text-center">
+        You’ve successfully created a project with
+        <a
+          class="tw:text-blue-600 tw:font-medium tw:hover:underline"
+          href="https://vite.dev/"
+          target="_blank"
+          rel="noopener"
+          >Vite</a
+        >
+        +
+        <a
+          class="tw:text-emerald-600 tw:font-medium tw:hover:underline"
+          href="https://vuejs.org/"
+          target="_blank"
+          rel="noopener"
+          >Vue 3</a
+        >. What's next?
+      </p>
+    </div>
+  </div>
+
   <hr class="tw:opacity-10 tw:my-4" />
 
   <h2 class="tw:text-center tw:mb-4 tw:text-gray-800">Elysia API Response</h2>
