0fa94e3ebf4ea7a1f715ec554c72b10410d451cc
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2025-01-31 18:04:26 +0700

    feat(web): add quasar

--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -2,6 +2,10 @@ import './assets/main.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { Quasar } from 'quasar';
+
+import '@quasar/extras/material-icons/material-icons.css';
+import 'quasar/src/css/index.sass';
 
 import App from './App.vue';
 import router from './router';
@@ -9,6 +13,9 @@ import router from './router';
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(Quasar, {
+  plugins: {},
+});
 app.use(router);
 
 app.mount('#app');
